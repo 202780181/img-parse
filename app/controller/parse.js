eaf08430d7a943fbfc63288326e9efb1b7512ace
Aaron <202780181@.qqcom>
2021-07-19 22:32:50 +0800

    更新修复可选Blob 数据

--- app/controller/parse.js
+++ app/controller/parse.js
@@ -12,36 +12,31 @@ const task = (url) => {
 	return source._url;
 }
 
-// internal url to file stream
-const getFileStrem = async (file, isFile) => {
+
+/* internal url to file stream */
+const getBlob = async (file, isFile) => {
 	const source = await task(file, isFile);
 	const type = source.split('.')[1];
-	console.log('成功回调=======');
-	console.log(source);
-	const fileStrem = https.get(source, (res)=> {
-		var chunks = []; //用于保存网络请求不断加载传输的缓冲数据
-		var size = 0;　　 //保存缓冲数据的总长度
-		res.on('data', function (chunk) {
-			chunks.push(chunk);
-			//累加缓冲数据的长度
-			size += chunk.length;
-		});
-		res.on('end', function (err) {
-			//Buffer.concat将chunks数组中的缓冲数据拼接起来，返回一个新的Buffer对象赋值给data
-			var data = Buffer.concat(chunks, size);
-			//可通过Buffer.isBuffer()方法判断变量是否为一个Buffer对象
-			console.log(Buffer.isBuffer(data));
-			//将Buffer对象转换为字符串并以base64编码格式显示
-			const base64Img = data.toString('base64');
-			//进入终端terminal,然后进入index.js所在的目录，
-			return base64Img
-		});
+	let fileStream = new Promise((resolve, reject) => {
+		https.get(source, res => {
+			// 二进制
+			res.setEncoding('binary');
+			let files = '';
+			// 加载到内存
+			res.on('data', chunk => {
+				files += chunk;
+			}).on('end', () => {
+				resolve(files);
+			})
+		})
 	})
-	console.log('输出文件=======');
-	console.log(fileStrem);
-
+	let binaryFiles = await fileStream.then(data => {
+		return data
+	})
+	return binaryFiles
 }
 
+/* control class */
 class ImageParse extends Controller {
 	async info() {
 		const { ctx, service } = this;
@@ -56,22 +51,20 @@ class ImageParse extends Controller {
 			} catch (err) {
 				status = 500;
 			}
-
-			ctx.status = status,
-			ctx.body = {
-				file: source
-			}
 		} else {
 			const {file, isFile = false} = ctx.request.body;
-			let fileStrem = await getFileStrem(file, isFile);
-			console.log('打印压缩后输出======');
-			console.log(fileStrem);
-			if(source) {
-				ctx.status = status;
-				ctx.body = {
-					file: fileStrem,
-					state: 0
-				}
+			let fileStream;
+			if(isFile) {
+				// Blob Object
+				fileStream = await getBlob(file, isFile);
+			} else {
+				// url address
+				fileStream = await task(file);
+			}
+			ctx.status = status;
+			ctx.body = {
+				file: fileStream,
+				state: 0
 			}
 		}
 	}
