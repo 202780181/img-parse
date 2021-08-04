ce4194d32c39c58d491d4461763e5f39062c1d71
longyadong <202780181@qq.com>
2021-08-04 09:35:14 +0800

    fix: update

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# encoding: utf-8.0
+
+# Exit on error
+set -o errexit
+
+hosts="172.0.0.1"
+port=21
+user=admin
+pass=admin123
+work_path="/root/img-parse"
+project="img-parse"
+
+
+
+# 推送到 aliyun 服务器
+# nodejs 服务不需要打包
+
+
